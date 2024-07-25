1229748e1efa0f1bca09ac86a8fef47a6d50af8a
Praveshkacode <pravesh182003@gmail.com>
2024-07-25 23:07:01 +0530

    fix the url /

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,6 +7,7 @@ const StoreContextProvider = (props) => {
 
     const[cartItems,setCartItems] = useState({});
     const url="https://bhojanbazaar-backend.onrender.com/"
+    
     const [token,setToken] = useState("")
     const [food_list,setFoodList] =useState([])
 
